4755fae5a2919f14726c4c8c1e3b284e5f01b301
Alex Vlasov <alex.m.vlasov@gmail.com>
2020-01-08 00:20:45 +0200

    also verbose compare ops

--- honggfuzz/run_compare_ops.sh
+++ honggfuzz/run_compare_ops.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cargo install honggfuzz
 cargo update
-NUM_JOBS=12 HFUZZ_RUN_ARGS="-u -n 32" cargo hfuzz run fuzz_target_compare_ops
+HFUZZ_RUN_ARGS="-u -V -Q -v -l log.log" cargo hfuzz run fuzz_target_compare_ops
