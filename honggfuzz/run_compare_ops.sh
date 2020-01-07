72707dd095d0900acf823e0d1cc6e32dbabf24fe
Alex Vlasov <alex.m.vlasov@gmail.com>
2020-01-08 01:14:47 +0200

    spawn more threads

--- honggfuzz/run_compare_ops.sh
+++ honggfuzz/run_compare_ops.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cargo install honggfuzz
 cargo update
-HFUZZ_RUN_ARGS="-u -V -Q --exit_upon_crash" cargo hfuzz run fuzz_target_compare_ops
+# HFUZZ_RUN_ARGS="-u -V -n 32 -Q --exit_upon_crash" cargo hfuzz run fuzz_target_compare_ops
+HFUZZ_RUN_ARGS="-u -V -n 32" cargo hfuzz run fuzz_target_compare_ops
