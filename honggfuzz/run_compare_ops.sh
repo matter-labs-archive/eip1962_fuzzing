c182759a00227473d530386b5f64a755b5fd453a
Alex Vlasov <alex.m.vlasov@gmail.com>
2020-01-08 00:42:30 +0200

    exit upon crash for now

--- honggfuzz/run_compare_ops.sh
+++ honggfuzz/run_compare_ops.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cargo install honggfuzz
 cargo update
-HFUZZ_RUN_ARGS="-u -V -Q" cargo hfuzz run fuzz_target_compare_ops
+HFUZZ_RUN_ARGS="-u -V -Q --exit_upon_crash" cargo hfuzz run fuzz_target_compare_ops
