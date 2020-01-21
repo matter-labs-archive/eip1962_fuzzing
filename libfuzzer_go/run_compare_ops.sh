441039b7bb55e55686c34c953a4b48dc6a6fe835
Alex Vlasov <alex.m.vlasov@gmail.com>
2020-01-21 19:41:45 +0300

    fix libfuzzer

--- libfuzzer_go/run_compare_ops.sh
+++ libfuzzer_go/run_compare_ops.sh
@@ -3,4 +3,7 @@ cargo install cargo-fuzz
 cd fuzz
 cargo update
 cd ..
-CC=clang CXX=clang++ NUM_JOBS=32 cargo +nightly fuzz run fuzz_target_compare_ops --jobs=32 -- -max_len=16000
+# CC=clang CXX=clang++ cargo +nightly fuzz run fuzz_target_compare_ops -- -max_len=16000
+CC=clang CXX=clang++ NUM_JOBS=8 cargo +nightly fuzz run fuzz_target_compare_ops --jobs=8 -- -max_len=16000
+#  -ignore_crashes=1
+# -help=1
