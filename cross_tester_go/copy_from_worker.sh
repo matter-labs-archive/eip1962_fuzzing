3e676459c8c02b91f7b90e72153450696731d511
Alex Vlasov <alex.m.vlasov@gmail.com>
2020-01-27 14:57:15 +0300

    update copy script

--- cross_tester_go/copy_from_worker.sh
+++ cross_tester_go/copy_from_worker.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-scp root@165.22.69.114:/root/eip1962_fuzzing/honggfuzz/hfuzz_workspace/fuzz_target_compare_ops/HONGGFUZZ.REPORT.TXT ./fuzz_vectors/
-scp root@165.22.69.114:/root/eip1962_fuzzing/honggfuzz/hfuzz_workspace/fuzz_target_compare_ops/*.fuzz ./fuzz_vectors/
+scp root@165.22.69.114:/root/eip1962_fuzzing/honggfuzz_go/hfuzz_workspace/fuzz_target_compare_ops/HONGGFUZZ.REPORT.TXT ./fuzz_vectors/
+scp root@165.22.69.114:/root/eip1962_fuzzing/honggfuzz_go/hfuzz_workspace/fuzz_target_compare_ops/*.fuzz ./fuzz_vectors/
