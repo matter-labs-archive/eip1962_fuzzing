21a9e9b32871a8e374472590f796e1d228cc0828
Alex Vlasov <alex.m.vlasov@gmail.com>
2020-01-31 19:50:43 +0300

    run libfuzzer in release

--- libfuzzer/run_compare_ops.sh
+++ libfuzzer/run_compare_ops.sh
@@ -3,4 +3,4 @@ cargo install cargo-fuzz
 cd fuzz
 cargo update
 cd ..
-CC=clang CXX=clang++ NUM_JOBS=32 cargo +nightly fuzz run fuzz_target_compare_ops --jobs=32 -- -max_len=16000
+CC=clang CXX=clang++ NUM_JOBS=32 cargo +nightly fuzz run --release fuzz_target_compare_ops --jobs=32 -- -max_len=16000
--- libfuzzer_go/run_fuzz_compare.sh
+++ libfuzzer_go/run_fuzz_compare.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-CC=clang CXX=clang++ NUM_JOBS=12 cargo +nightly fuzz run fuzz_target_compare --jobs=12 -- -max_len=8192
+CC=clang CXX=clang++ NUM_JOBS=12 cargo +nightly fuzz run --release fuzz_target_compare --jobs=12 -- -max_len=8192
