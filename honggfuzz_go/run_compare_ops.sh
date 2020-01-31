3dadedfb4ed1d9eba7e8f065f217b9652a213ee3
Alex Vlasov <alex.m.vlasov@gmail.com>
2020-01-31 20:10:08 +0300

    allow to test on larger input size

--- honggfuzz_go/run_compare_ops.sh
+++ honggfuzz_go/run_compare_ops.sh
@@ -5,4 +5,4 @@ cargo install honggfuzz
 # cargo clean eth_pairings_go
 cargo update
 # HFUZZ_RUN_ARGS="-u -V -n 32 -Q --exit_upon_crash" cargo hfuzz run fuzz_target_compare_ops
-HFUZZ_RUN_ARGS="-u -V -n 32 --tmout_sigvtalrm" cargo hfuzz run fuzz_target_compare_ops
+HFUZZ_RUN_ARGS="-u -V -n 32 --tmout_sigvtalrm --max_file_size 16000" cargo hfuzz run fuzz_target_compare_ops
