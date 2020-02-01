69319634982261943e002f8a8e3c3ba27fb2e852
Alex Vlasov <alex.m.vlasov@gmail.com>
2020-02-01 15:43:16 +0300

    run limited number of workers to completion

--- libfuzzer_go/run_compare_ops.sh
+++ libfuzzer_go/run_compare_ops.sh
@@ -4,7 +4,7 @@ cd fuzz
 # cargo clean eth_pairings_go
 cargo update
 cd ..
-# CC=clang CXX=clang++ cargo +nightly fuzz run fuzz_target_compare_ops -- -max_len=16000 --jobs=10000
-CC=clang CXX=clang++ NUM_JOBS=10000 cargo +nightly fuzz run --release fuzz_target_compare_ops -- -max_len=16000
+# CC=clang CXX=clang++ cargo +nightly fuzz run fuzz_target_compare_ops -- -max_len=16000 
+CC=clang CXX=clang++ cargo +nightly fuzz run --release fuzz_target_compare_ops -- -max_len=16000 -jobs=1000 -workers=32
 # CC=clang CXX=clang++ cargo +nightly fuzz run --release fuzz_target_compare_ops -- -max_len=16000 -ignore_crashes=1 -fork=32 
 # -help=1
