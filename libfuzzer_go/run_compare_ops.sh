f0a271a8a6d2c43c631d442525ded421e3bcc552
Alex Vlasov <alex.m.vlasov@gmail.com>
2020-02-01 15:31:31 +0300

    full to 10k crashes

--- libfuzzer_go/run_compare_ops.sh
+++ libfuzzer_go/run_compare_ops.sh
@@ -5,6 +5,6 @@ cd fuzz
 cargo update
 cd ..
 # CC=clang CXX=clang++ cargo +nightly fuzz run fuzz_target_compare_ops -- -max_len=16000
-# CC=clang CXX=clang++ NUM_JOBS=8 cargo +nightly fuzz run --release fuzz_target_compare_ops --jobs=8 -- -max_len=16000 -ignore_crashes=1
-CC=clang CXX=clang++ cargo +nightly fuzz run --release fuzz_target_compare_ops -- -max_len=16000 -ignore_crashes=1 -fork=32 
+CC=clang CXX=clang++ cargo +nightly fuzz run --release fuzz_target_compare_ops --jobs=10000 -- -max_len=16000
+# CC=clang CXX=clang++ cargo +nightly fuzz run --release fuzz_target_compare_ops -- -max_len=16000 -ignore_crashes=1 -fork=32 
 # -help=1
