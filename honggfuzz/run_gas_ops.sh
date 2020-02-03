75f6be79b55f54603890881a980957bab137f064
Alex Vlasov <alex.m.vlasov@gmail.com>
2020-02-03 16:13:36 +0300

    fix honggfuzz target for gas

--- honggfuzz/run_gas_ops.sh
+++ honggfuzz/run_gas_ops.sh
@@ -2,4 +2,4 @@
 cargo install honggfuzz
 # cargo install --git https://github.com/rust-fuzz/honggfuzz-rs.git
 cargo update
-HFUZZ_RUN_ARGS="-u -V -n 32" cargo hfuzz run fuzz_target_gas
+HFUZZ_RUN_ARGS="-u -V -n 32 --tmout_sigvtalrm --max_file_size 16000" cargo hfuzz run fuzz_target_gas
