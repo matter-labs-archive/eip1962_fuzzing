bebba5d5d0a0b45f45b1b9068c4d5d1af91950a5
Alex Vlasov <alex.m.vlasov@gmail.com>
2020-02-06 17:50:59 +0300

    add utility tool to clear fuzz vectors

--- cross_tester/copy_from_worker.sh
+++ cross_tester/copy_from_worker.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 scp root@165.22.69.114:/root/eip1962_fuzzing/honggfuzz/hfuzz_workspace/fuzz_target_compare_ops/HONGGFUZZ.REPORT.TXT ./fuzz_vectors/
 scp root@165.22.69.114:/root/eip1962_fuzzing/honggfuzz/hfuzz_workspace/fuzz_target_compare_ops/*.fuzz ./fuzz_vectors/
+scp root@165.22.69.114:/root/eip1962_fuzzing/honggfuzz/hfuzz_workspace/fuzz_target_compare_ops/*.verified ./fuzz_vectors/
---
+++ honggfuzz/clear_vectors.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+rm hfuzz_workspace/fuzz_target_compare_ops/*.verified
+rm hfuzz_workspace/fuzz_target_compare_ops/*.fuzz
